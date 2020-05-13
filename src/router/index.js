a12abc08860dae80f634e6f20919de843e0e8a77
dagnyjones <dagny.jons5@gmail.com>
2020-05-13 06:17:50 +0200

    ..

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ import Admin from '../views/Admin.vue'
 import AddNewItems from '../components/admin/AddNewItems.vue'
 import Login from '../components/admin/Login.vue'
 
+import firebase from 'firebase'
+import 'firebase/firestore'
+
 
 Vue.use(VueRouter)
 
@@ -31,12 +34,18 @@ Vue.use(VueRouter)
   {
     path: '/admin',
     name: 'Admin',
-    component: Admin
+    component: Admin,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/addNew',
     name: 'addNew',
-    component: AddNewItems
+    component: AddNewItems,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/login',
@@ -54,6 +63,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login');
+  else next();
+});
 
 export default router
